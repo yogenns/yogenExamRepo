110cf93ba5ff600e5b12ef7fbab8b1c1165b1968
yogenns <yogenns@gmail.com>
2020-08-26 11:44:15 +0530

    Task 2 - Create Login Page

--- yogenExamTool/exam_app/urls.py
+++ yogenExamTool/exam_app/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'exam_app'
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url('login/', auth_views.LoginView.as_view(template_name='exam_app/login.html'), name='login'),
 ]
--- yogenExamTool/yogenExamTool/settings.py
+++ yogenExamTool/yogenExamTool/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'exam_app',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = 'exam_app:home'
+LOGOUT_REDIRECT_URL = 'exam_app:home'
