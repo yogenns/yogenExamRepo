e2c64173f63e2180eab507c914bc2f7b1da06566
yogenns <yogenns@gmail.com>
2020-09-01 17:28:05 +0530

    Task 18 - Create Exam Attempt Model

--- yogenExamTool/exam_app/models.py
+++ yogenExamTool/exam_app/models.py
@@ -1,6 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -40,3 +40,23 @@ class Exam(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ExamAttempt(models.Model):
+    RESULT_STATUS = (
+        ('P', 'PASS'),
+        ('F', 'FAIL')
+    )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    answer_list = models.TextField()
+    result_attempted_question_count = models.PositiveSmallIntegerField()
+    result_correct_answer_count = models.PositiveSmallIntegerField()
+    result_percentage = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(100), MinValueValidator(0)])
+    _result_status = models.CharField(max_length=1, choices=RESULT_STATUS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+    def result_status(self):
+        return (dict(self.RESULT_STATUS))[self._result_status]
